4ec67d509351cc53bd08fd409d5457fa19c53a91
sdesai <sjdesai16@users.noreply.github.com>
2014-09-06 13:06:45 -0400

    Adding Sources

---
+++ code/jobs1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cygstart /bin/bash -li
+cd c:/users/vivek.apurv.Apurv-PC/Desktop/PDCA_Update
+java -jar client.jar localhost 51000 jobs.jar jobs.Hello
+
+cygstart /bin/bash -li
+cd c:/users/vivek.apurv.Apurv-PC/Desktop/PDCA_Update
+java -jar client.jar localhost 51000 jobs.jar jobs.Mvm
+
+
+./job1.sh
